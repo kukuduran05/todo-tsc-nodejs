a46ba9aa526b008765e3ea8c95fc3477fa761826
Karla Janette Solis Duran <karla.solis@Encoras-MacBook-Pro.local>
2021-06-23 09:37:16 -0500

    database updated, I added foreign keys for userId and categoryId

--- src/sql/database.sql
+++ src/sql/database.sql
@@ -1,32 +1,41 @@
 CREATE DATABASE todo_nodejs;
 
-CREATE TABLE users (
-    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(200) NOT NULL,
-    lastname VARCHAR(200),
-    email   text NOT NULL,
-    password VARCHAR(100) NOT NULL,
-    created_at timestamp NULL DEFAULT NULL
-);
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `userId` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `lastname` varchar(200) DEFAULT NULL,
+  `email` text NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DESCRIBE users;
 
-CREATE TABLE tasks (
-  id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title varchar(100) NOT NULL DEFAULT '',
-  description text,
-  categoryId int DEFAULT NULL,
-  userId int DEFAULT NULL,
-  created_at timestamp NULL DEFAULT NULL
-);
-
-DESCRIBE tasks;
-
-CREATE TABLE categories (
-  id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title varchar(100) NOT NULL DEFAULT '',
-  description text,
-  created_at timestamp NULL DEFAULT NULL
-);
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
+  `categoryId` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL DEFAULT '',
+  `description` text,
+  `created_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`categoryId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DESCRIBE categories;
+
+DROP TABLE IF EXISTS `tasks`;
+  `taskId` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL DEFAULT '',
+  `description` text,
+  `categoryId` int DEFAULT NULL,
+  `userId` int DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`taskId`),
+  KEY `fk_user_id_idx` (`userId`),
+  KEY `fk_category_id_idx` (`categoryId`),
+  CONSTRAINT `fk_category_id` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`categoryId`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DESCRIBE tasks;
