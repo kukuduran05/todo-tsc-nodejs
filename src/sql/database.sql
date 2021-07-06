28eb9be96e6b845b0e159d880df1d6864301e09d
Karla Janette Solis Duran <karla.solis@Encoras-MacBook-Pro.local>
2021-07-06 13:47:33 -0500

    userId rename field in database file

--- src/sql/database.sql
+++ src/sql/database.sql
@@ -8,27 +8,27 @@ CREATE TABLE `users` (
   `email` text NOT NULL,
   `password` varchar(100) NOT NULL,
   PRIMARY KEY (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=114 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE `categories` (
   `categoryId` int NOT NULL AUTO_INCREMENT,
   `title` varchar(100) NOT NULL DEFAULT '',
   `description` text,
-  `userId` int DEFAULT NULL,
+  `userUserId` int DEFAULT NULL,
   PRIMARY KEY (`categoryId`),
-  KEY `fk_users_cats_idx` (`userId`),
-  CONSTRAINT `fk_users_cats` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `fk_users_cats_idx` (`userUserId`),
+  CONSTRAINT `fk_users_cats` FOREIGN KEY (`userUserId`) REFERENCES `users` (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `tasks`;
 CREATE TABLE `tasks` (
   `taskId` int NOT NULL AUTO_INCREMENT,
   `title` varchar(100) NOT NULL DEFAULT '',
   `description` text,
-  `userId` int DEFAULT NULL,
+  `userUserId` int DEFAULT NULL,
   `categories` varchar(500) DEFAULT NULL,
   PRIMARY KEY (`taskId`),
-  KEY `fk_users_tasks_idx` (`userId`),
-  CONSTRAINT `fk_users_tasks` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `fk_users_tasks_idx` (`userUserId`),
+  CONSTRAINT `fk_users_tasks` FOREIGN KEY (`userUserId`) REFERENCES `users` (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
