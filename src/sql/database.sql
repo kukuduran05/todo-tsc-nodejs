4e2b97889be7cc6e3999cc6cac3a616f9e23c7dd
Karla Janette Solis Duran <karla.solis@Encoras-MacBook-Pro.local>
2021-07-15 15:28:48 -0500

    database.sql file was updated to new relational database

--- src/sql/database.sql
+++ src/sql/database.sql
@@ -6,9 +6,9 @@ CREATE TABLE `users` (
   `name` varchar(200) NOT NULL,
   `lastname` varchar(200) DEFAULT NULL,
   `email` text NOT NULL,
-  `password` varchar(100) NOT NULL,
+  `password` varchar(300) NOT NULL,
   PRIMARY KEY (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=114 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=127 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE `categories` (
@@ -19,16 +19,25 @@ CREATE TABLE `categories` (
   PRIMARY KEY (`categoryId`),
   KEY `fk_users_cats_idx` (`userUserId`),
   CONSTRAINT `fk_users_cats` FOREIGN KEY (`userUserId`) REFERENCES `users` (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `tasks`;
 CREATE TABLE `tasks` (
   `taskId` int NOT NULL AUTO_INCREMENT,
   `title` varchar(100) NOT NULL DEFAULT '',
   `description` text,
-  `userUserId` int DEFAULT NULL,
-  `categories` varchar(500) DEFAULT NULL,
+  `userUserId` int NOT NULL,
   PRIMARY KEY (`taskId`),
   KEY `fk_users_tasks_idx` (`userUserId`),
   CONSTRAINT `fk_users_tasks` FOREIGN KEY (`userUserId`) REFERENCES `users` (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=71 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DROP TABLE IF EXISTS `tasks_categories_categories`;
+CREATE TABLE `tasks_categories_categories` (
+  `categoriesCategoryId` int NOT NULL,
+  `tasksTaskId` int NOT NULL,
+  PRIMARY KEY (`categoriesCategoryId`,`tasksTaskId`),
+  KEY `fk_tasks_idx` (`tasksTaskId`),
+  CONSTRAINT `fk_categories` FOREIGN KEY (`categoriesCategoryId`) REFERENCES `categories` (`categoryId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_tasks` FOREIGN KEY (`tasksTaskId`) REFERENCES `tasks` (`taskId`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
